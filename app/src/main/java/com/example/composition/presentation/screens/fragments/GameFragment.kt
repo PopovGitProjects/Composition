9818bc8afd18923ccdb1248625ac5de6f14a535c
PopovGitProjects <popov.workmail@gmail.com>
2023-02-16 18:01:03 +0500

    Added GameFragment.kt

--- app/src/main/java/com/example/composition/presentation/screens/fragments/GameFragment.kt
+++ app/src/main/java/com/example/composition/presentation/screens/fragments/GameFragment.kt
@@ -7,13 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.composition.R
 
-class ChooseLevelFragment : Fragment() {
+class GameFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_choose_level, container, false)
+        return inflater.inflate(R.layout.fragment_game, container, false)
     }
 }
